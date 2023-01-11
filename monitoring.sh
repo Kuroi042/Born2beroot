9dbb99585d43008f7c5ce12c815c2f6da54703ee
MedCharaf <bouderraoui45@gmail.Com>
2023-01-11 15:26:58 +0100

    Create monitoring.sh
    monitoring script for b2broot project

---
+++ monitoring.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo  "#Architecture: $(uname -a)"
+echo  "#CPU physical : $(lscpu  | grep Socket | awk '{print $2}')"
+echo  "#vCPU : $(cat  /proc/cpuinfo | grep 'cpu cores' | awk ' NR == 1 {print $4}')"
+fram=$(free -m | awk 'NR == 2 {print $2}')
+uram=$(free -m | awk 'NR == 2 {print $3}')
+pram=$(free | awk 'NR == 2 {printf("%.2f"), $3/$2*100}')
+echo  "#Memory Usage: $uram/${fram}MB ($pram%)"
+echo  "#Disk Usage: $(df -m | grep "/dev/" | grep -v "boot" | awk '{avai += $2} {use += $3} END  {printf("%d/%dGB (%.2f%%)"), use ,avai/1024 ,(use*100)/avai}')"
+echo  "#CPU load: $(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')"
+echo  "#Last boot: $(who -b | awk 'NR == "1" {print $3 " " $4}')"
+var=$(lsblk | grep lvm | wc -l)
+echo  "#LVM use: $(if [ $var -gt 0 ]
+then
+        echo yes
+else
+        echo no
+        fi)"
+echo "#Connections TCP : $(cat /proc/net/sockstat{,6} | awk 'NR == "1" {print  $3}')" ESTABLISHED""
+echo "#user log : $(users | wc -w)"
+echo "#Network: IP $(hostname -I) ($(ip link show  | awk ' NR == 4 {print $2}'))"
+echo "#Sudo : $(journalctl -q _COMM=sudo | grep COMMAND | wc -l )" "cmd"
